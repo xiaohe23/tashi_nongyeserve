6fbdb688b8ad3de89c07333874bdc8d600c05cb2
menglinanke <486158409@qq.com>
2023-05-06 17:45:45 +0800

    5/6配置不同环境，loading加载效果

--- nongye-0301/src/utils/request.js
+++ nongye-0301/src/utils/request.js
@@ -1,22 +1,42 @@
+console.log(process.env.VUE_APP_API_URL);
+
 import axios from 'axios'
-import {Message} from 'element-ui'
+import Vue from 'vue';
+import {Loading,Message} from 'element-ui'
+import store from '@/store';
 const service= axios.create({
-    baseURL:"http://localhost:3007",
+    baseURL:process.env.VUE_APP_API_URL,
     timeout:10000
 })
+
+var loadObj = null
 //添加拦截器
-service.interceptors.request.use(
-    config=>{
-        return config
-    },
-    err=>{
-        return Promise.reject(err)
+service.interceptors.request.use( (config)=> {
+    // 设置旋转小图标
+    if (!loadObj) {
+        loadObj = Loading.service({
+            text: "加载中...",
+            spinner: "el-icon-loading",
+            background: "rgba(0,0,0,0.5)",
+            lock: true
+        })
     }
-)
+    // token
+    if (store.getters.token !== "") {
+        config.headers['user-token'] = store.getters.token;
+    }
+    return config;
+}, (error)=> {//失败
+    return Promise.reject(error);
+})
 
 service.interceptors.response.use(function (res) {
     // console.log(27, res);
     // 报错信息
+    Vue.nextTick(() => {
+        loadObj.close();
+        loadObj = null
+    })
     if (res.data.code !== 666) {//报错
         Message({
             message: res.data.msg, type: "error"
